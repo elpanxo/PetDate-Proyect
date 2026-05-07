ebaff258775d316d3268e232fea957d135af1a6f
EliecerSH <elie.salgado@duocuc.cl>
2026-05-07 13:12:04 -0400

    integracion del ms-mascota

---
+++ Proyecto/Backend/ms-mascotas/src/main/java/cl/PetDate/ms_mascotas/MsMascotasApplication.java
@@ -0,0 +1,15 @@
+package cl.PetDate.ms_mascotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class MsMascotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMascotasApplication.class, args);
+	}
+
+}
---
+++ Proyecto/Backend/ms-mascotas/src/main/java/cl/PetDate/ms_mascotas/clients/UsuarioClient.java
@@ -0,0 +1,16 @@
+package cl.PetDate.ms_mascotas.clients;
+
+import cl.PetDate.ms_mascotas.clients.dto.UsuarioDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(
+        name = "ms-usuarios",
+        url = "http://localhost:8081"
+)
+public interface UsuarioClient {
+
+    @GetMapping("/usuarios/{id}")
+    UsuarioDTO buscarUsuarioPorId(@PathVariable Long id);
+}
